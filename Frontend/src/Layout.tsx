11b0641181d3dedb8191a8ae053147307ac28054
Apoorva Rajpurohit <apoorva.b.rajpurohit@gmail.com>
2025-07-25 17:58:15 +0530

    Update Layout.tsx

--- Frontend/src/Layout.tsx
+++ Frontend/src/Layout.tsx
@@ -1,18 +1,13 @@
 import { Outlet } from "react-router";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 export default function Layout() {
 	return (
 		<div className="bg-blue-200/40 min-h-dvh max-w-screen-2xl flex flex-col items-center text-black">
 			<NavBar />
 			<div className=" my-15 h-full w-full px-5">{<Outlet />}</div>
-			<div className="mb-4">
-				Made with ⚡ by{" "}
-				<a href="https://donaldreddy.xyz" target="_blank" className="underline	">
-					Donald Reddy
-				</a>{" "}
-				{new Date().getFullYear()}
-			</div>
+			<Footer/>
 		</div>
 	);
 }
