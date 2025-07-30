455f43980a1f0c4aafcfb2946d44f5d6c9dea983
VaibhaviSuvarna <vaibhavisuvarna7@gmail.com>
2025-07-30 22:50:53 +0530

    Redesigned the entire home page by adding the hero section ,categories section and a contact us feedback section

--- Frontend/src/Layout.tsx
+++ Frontend/src/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
 	return (
 		<div className="bg-blue-200/40 min-h-dvh max-w-screen-2xl flex flex-col items-center text-black">
 			<NavBar />
-			<div className=" my-15 h-full w-full px-5">{<Outlet />}</div>
+			<div className=" my-15 h-full w-full ">{<Outlet />}</div>
 			<div className="mb-4">
 				Made with ⚡ by{" "}
 				<a href="https://donaldreddy.xyz" target="_blank" className="underline	">
