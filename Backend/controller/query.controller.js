1b128bc213cce1a2632089ab9e9e90a9174ecf3f
DonaldReddy <donaldreddy2712@gmail.com>
2025-08-06 13:12:01 +0530

    fic build errors

---
+++ Backend/controller/query.controller.js
@@ -0,0 +1,27 @@
+import prisma from "../database/dbConnect.js";
+
+class QueryController {
+	async createQuery(req, res) {
+		try {
+			const { username, email, query } = req.body;
+
+			if (!username || !email || !query) {
+				return res.status(400).json({ message: "All fields are required." });
+			}
+
+			const newQuery = await prisma.query.create({
+				data: { username, email, query },
+			});
+
+			res.status(201).json({
+				message: "Query successfully created",
+				data: newQuery,
+			});
+		} catch (error) {
+			console.error("Error creating query:", error);
+			res.status(500).json({ message: "Server error" });
+		}
+	}
+}
+
+export const queryController = new QueryController();
